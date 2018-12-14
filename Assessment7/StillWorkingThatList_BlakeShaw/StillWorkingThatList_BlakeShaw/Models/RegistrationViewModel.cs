53dc81ce1eef6cb165593a8f9d92f8fe59085d41
Blake McMillan Shaw <blakeshaw@quickenloans.com>
2018-12-14 14:05:07 -0500

    Added identity nugets, owin startup, registration and login models

---
+++ Assessment7/StillWorkingThatList_BlakeShaw/StillWorkingThatList_BlakeShaw/Models/RegistrationViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StillWorkingThatList_BlakeShaw.Models
+{
+    public class RegistrationViewModel
+    {
+        [Required, Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required, Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Preferred Date"), DataType(DataType.Date)]
+        public Nullable<System.DateTime> AttendanceDate { get; set; }
+        [Required]
+        public string Attending { get; set; }
+        [Display(Name = "Email"), EmailAddress]
+        public string EmailAddress { get; set; }
+        [Display(Name = "Guest Name (if applicable)")]
+        public string Guest1 { get; set; }
+        [Required, StringLength(100, MinimumLength = 6, ErrorMessage = "The {0} must be at least {2} characters long.")]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password"), Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Assessment7/StillWorkingThatList_BlakeShaw/StillWorkingThatList_BlakeShaw/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Owin;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(StillWorkingThatList_BlakeShaw.Startup))]
+
+namespace StillWorkingThatList_BlakeShaw
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BlakeIdentityPartyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            app.CreatePerOwinContext(() => new IdentityDbContext(connectionString));
+
+            app.CreatePerOwinContext<UserManager<IdentityUser>>((options, context) =>
+                new UserManager<IdentityUser>(
+                    new UserStore<IdentityUser>(context.Get<IdentityDbContext>())));
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Identity/Login"),
+            });
+        }
+    }
+}
